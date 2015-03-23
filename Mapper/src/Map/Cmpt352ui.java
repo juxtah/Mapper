35574a53060f19d17fc612f2a27581a9a9ac9832
NguX <NguX@uofs-10-227-142-131.usask.ca>
2015-03-23 10:47:23 -0600

    minor changes on input method. now adds line on map and adjust points coordinates for more precise positioning

--- Mapper/src/Map/Cmpt352ui.java
+++ Mapper/src/Map/Cmpt352ui.java
@@ -20,7 +20,7 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -48,15 +48,16 @@ public class Cmpt352ui extends Application {
         VBox startingTimePanel = new VBox();
         VBox endingTimePanel = new VBox();
         VBox submitPanel = new VBox();
-        StackPane holder = new StackPane();
+        Pane holder = new Pane();
 
-        controlPanel.setSpacing(20);
+        controlPanel.setSpacing(30);
 
         devicePanel.setSpacing(10);
         Label deviceLabel = new Label("Device list: ");
         deviceLabel.setFont(Font.font("Arial", 14));
         ChoiceBox <String> deviceList = new ChoiceBox<>();
         deviceList.setItems(myModel.getDevice());
+        deviceList.setPrefWidth(350);
 
         startingTimePanel.setSpacing(10);
         Label startingTimeLabel = new Label("Starting Time:");
@@ -65,35 +66,39 @@ public class Cmpt352ui extends Application {
         TextField startingHr = new TextField("9");
         startingHr.setPrefWidth(40);
         Label startingHrLabel = new Label("Hr");
+        startingHrLabel.setPadding(new Insets(5, 0 ,0 ,0));
         TextField startingMin = new TextField("30");
         startingMin.setPrefWidth(40);
         Label startingMinLabel = new Label("Min");
+        startingMinLabel.setPadding(new Insets(5, 0 ,0 ,0));
         TextField startingDay = new TextField("23");
         startingDay.setPrefWidth(40);
         ChoiceBox<String> startingMonth = new ChoiceBox<>();
         startingMonth.getItems().addAll("January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-        startingMonth.getSelectionModel().select(3);
+        startingMonth.getSelectionModel().select(2);
         TextField startingYear = new TextField("2015");
-        startingYear.setMaxWidth(60);
+        startingYear.setMaxWidth(50);
         startingTimeDate.getChildren().addAll(startingHr, startingHrLabel, startingMin, startingMinLabel, startingDay, startingMonth, startingYear);
 
         endingTimePanel.setSpacing(10);
         Label endingTimeLabel = new Label("Ending Time:");
         HBox endingTimeDate = new HBox();
         endingTimeDate.setSpacing(5);
-        TextField endingHr = new TextField("20");
+        TextField endingHr = new TextField("22");
         endingHr.setPrefWidth(40);
         Label endingHrLabel = new Label("Hr");
+        endingHrLabel.setPadding(new Insets(5, 0 ,0 ,0));
         TextField endingMin = new TextField("00");
         endingMin.setPrefWidth(40);
         Label endingMinLabel = new Label("Min");
+        endingMinLabel.setPadding(new Insets(5, 0 ,0 ,0));
         TextField endingDay = new TextField("23");
         endingDay.setPrefWidth(40);
         ChoiceBox<String> endingMonth = new ChoiceBox<>();
         endingMonth.getItems().addAll("January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-        endingMonth.getSelectionModel().select(3);
+        endingMonth.getSelectionModel().select(2);
         TextField endingYear = new TextField("2015");
-        endingYear.setMaxWidth(60);
+        endingYear.setMaxWidth(50);
         endingTimeDate.getChildren().addAll(endingHr, endingHrLabel, endingMin, endingMinLabel, endingDay, endingMonth, endingYear);
 
         submitPanel.setPadding(new Insets(25, 0, 0, 0));
@@ -134,8 +139,15 @@ public class Cmpt352ui extends Application {
     
     private void drawRoute(GraphicsContext gc){
         gc.clearRect(0, 0, 1280, 720);
+        GPSSet<Date, Double, Double> prevGPS = null;
         for(GPSSet<Date, Double, Double> i : toDraw){
-            gc.fillOval(i.getLongitude(), i.getLatitude(), 5, 5);
+            gc.fillOval(i.getLongitude()-2.5, i.getLatitude()-2.5, 5, 5);
+            if(prevGPS==null){
+                prevGPS = i;
+            }else{
+                gc.strokeLine(i.getLongitude(), i.getLatitude(), prevGPS.getLongitude(), prevGPS.getLatitude());
+                prevGPS = i;
+            }
         }
     }
 
